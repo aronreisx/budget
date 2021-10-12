ae4d9069fb0baac3dc29b2baf12d6b869bedf6f2
Aron Reis <aronreis2@gmail.com>
2021-10-12 15:23:40 +0100

    :sparkles: Component Dashboard

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Summary } from '../Summary';
+import { TransactionTable } from '../TransactionsTable';
+import { Container } from './styles';
+
+export function Dashboard() {
+    return(
+        <Container>
+            <Summary/>
+            <TransactionTable/>
+        </Container>
+    );
+};
